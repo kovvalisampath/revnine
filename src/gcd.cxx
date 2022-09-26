98026e219891a82a118d605436a9b5163bfd1393
sampath <113584187+sampath017@users.noreply.github.com>
2022-09-26 14:54:03 +0530

    redefined

--- src/gcd.cxx
+++ src/gcd.cxx
@@ -18,8 +18,8 @@ int64_t gcd(int64_t a, int64_t b)
 
 	else
 	{
-		int64_t new_a = NULL;
-		int64_t new_b = NULL;
+		int64_t new_a = 0;
+		int64_t new_b = 0;
 
 		while (b != 0)
 		{
@@ -32,6 +32,8 @@ int64_t gcd(int64_t a, int64_t b)
 
 		return a;
 	}
+
+	return -1;
 }
 
 int64_t gcd(int64_t a, int64_t b, int64_t& x, int64_t& y)
@@ -44,8 +46,8 @@ int64_t gcd(int64_t a, int64_t b, int64_t& x, int64_t& y)
 		return a;
 	}
 
-	int64_t new_x = NULL;
-	int64_t new_y = NULL;
+	int64_t new_x = 0;
+	int64_t new_y = 0;
 
 	int64_t divisor = gcd(b, a % b, new_x, new_y);
 
---
+++ src/revnine.cxx
@@ -0,0 +1,25 @@
+﻿#include "revnine.hxx"
+
+/**
+ * @brief testing.
+ */
+
+int main()
+{
+    int64_t x = 0;
+    int64_t y = 0;
+    int64_t a = 34;
+    int64_t b = 126;
+
+    if (a == 0 && b == 0)
+    {
+        std::cout << "Enter a, b: ";
+        std::cin >> a >> b;
+    }
+
+    if (a < b) std::swap(a, b);
+
+    std::cout << "The gcd of " << a << ", " << b << "is: " << gcd(a, b, x, y) << std::endl;
+    std::cout << x << ", " << y << std::endl;
+
+}
